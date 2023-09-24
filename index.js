b066b6599e838fb395a4b3ff75a965dc0438696a
aydinonac <aydinonac@yahoo.com>
2023-09-24 14:55:18 +0100

    tidied up

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ const saltRounds = 10
 const secretKey = "aydin is cool";
 
 const users = []
+
 app.use(express.json())
 
-app.get('/hello', async(req,res) => {
-    res.send("Hello from Aydin")
-})
+app.get('/hello', async(req,res) => res.send("Hello from A"))
 
 app.post("/register", async(req, res) => {
     const {email, pwd} = req.body
     const hashedPwd = await bcrypt.hash(pwd, saltRounds)
-    users.push({email, pwd:hashedPwd})
-    res.status(201).send({message: `Registered successfully!${email}${hashedPwd}`})
+    users.push({email, pwd: hashedPwd})
+    res.status(201).send({message: `Registered successfully!   Your email is: ${email}   Hashed version is: ${hashedPwd}`})
 })
+
 app.post("/login", async(req, res) => {
     const {email, pwd} = req.body
     const user = users.find(u => u.email === email)
@@ -34,21 +34,15 @@ app.post("/login", async(req, res) => {
 
 // middleware function for priviledged acces
 function authenticateToken (req, res, next) {
-    const authHeader = req.headers["author"]
-
-    const token = authHeader
-    if (token == null) return res.sendStatus(401)
-    jwt.verify(token, secretKey, (err, user) => {
+    const authHeaderToken = req.headers["author"]
+    if (authHeaderToken == null) return res.sendStatus(401)
+    jwt.verify(authHeaderToken, secretKey, (err, user) => {
         if (err) return res.sendStatus(403)
         req.user = user
-        // next()
+        next()
         })
 }
-app.get("/protected", authenticateToken, (req, res) => {
-    res.send("Protected route")
-})
+app.get("/protected", authenticateToken, (req, res) => res.send("Protected route"))
 
-app.listen(port, () => {
-    console.log("your server is running")
-})
+app.listen(port, () => console.log("your server is running"))
 
